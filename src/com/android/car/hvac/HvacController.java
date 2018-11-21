8114ba2cc766d9fb81def1d2593778ef8a320db8
Inseob Kim <inseob@google.com>
2018-11-21 13:38:16 +0900

    Schematize Car system properties
    Properties accessed across partitions are now schematized and will
    become APIs to make explicit interfaces among partitions.
    
    Bug: 117924132
    Test: mma -j
    Change-Id: Id8adcb18f5f1eaa2cd7b2c5ab3288f47d1fef4df
    Merged-In: Id8adcb18f5f1eaa2cd7b2c5ab3288f47d1fef4df

--- src/com/android/car/hvac/HvacController.java
+++ src/com/android/car/hvac/HvacController.java
@@ -27,7 +27,7 @@ import android.os.AsyncTask;
 import android.os.Binder;
 import android.os.Handler;
 import android.os.IBinder;
-import android.os.SystemProperties;
+import android.sysprop.CarProperties;
 import android.support.car.Car;
 import android.support.car.CarNotConnectedException;
 import android.support.car.CarConnectionCallback;
@@ -39,7 +39,6 @@ import java.util.List;
 import javax.annotation.concurrent.GuardedBy;
 
 public class HvacController extends Service {
-    private static final String DEMO_MODE_PROPERTY = "android.car.hvac.demo";
     private static final String TAG = "HvacController";
     private static final int DRIVER_ZONE_ID = VehicleAreaSeat.SEAT_ROW_1_LEFT |
             VehicleAreaSeat.SEAT_ROW_2_LEFT | VehicleAreaSeat.SEAT_ROW_2_CENTER;
@@ -120,7 +119,7 @@ public class HvacController extends Service {
     public void onCreate() {
         super.onCreate();
         if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_AUTOMOTIVE)) {
-            if (SystemProperties.getBoolean(DEMO_MODE_PROPERTY, false)) {
+            if (CarProperties.hvac_demo().orElse(false)) {
                 IBinder binder = (new LocalHvacPropertyService()).getCarPropertyService();
                 initHvacManager(new CarHvacManager(binder, this, new Handler()));
                 return;
